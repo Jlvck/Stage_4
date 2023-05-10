855a09365057c37c36aed2ed2ab7bc983e75740f
Jlvck <jonaruthan@gmail.com>
2023-05-10 20:51:38 +0100

    understanding http requests

--- flutter_complete_guide/lib/providers/products.dart
+++ flutter_complete_guide/lib/providers/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -56,6 +59,20 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    final url = Uri.parse(
+        'https://flutter-update-72c7a-default-rtdb.firebaseio.com/products.json');
+    http.post(
+      url,
+      body: json.encode(
+        {
+          'title': product.title,
+          'description': product.description,
+          'price': product.price,
+          'imageUrl': product.imageUrl,
+          'isFavorite': product.isFavorite,
+        },
+      ),
+    );
     final newProduct = Product(
         id: DateTime.now().toString(),
         title: product.title,
